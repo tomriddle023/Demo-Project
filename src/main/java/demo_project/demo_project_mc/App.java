51512c5780ad106c56271b7c04aa2dbbc5a1d94b
tomriddle023 <dark4lord23@gmail.com>
2018-11-15 18:05:33 +0530

    firsst commit

---
+++ src/main/java/demo_project/demo_project_mc/App.java
@@ -0,0 +1,13 @@
+package demo_project.demo_project_mc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/demo_project/demo_project_mc/tutorial1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo_project.demo_project_mc;
+
+/**
+ * @author sslindia
+ *
+ */
+public class tutorial1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" test  ddemo");
+	}
+
+}
