e982eb6ca22246c61c50c8cc48b8d3435b76a016
tomriddle023 <dark4lord23@gmail.com>
2018-11-15 18:13:59 +0530

    commit messsage

---
+++ src/main/java/demo_project/demo_project_mc/tutorial_2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package demo_project.demo_project_mc;
+
+/**
+ * @author sslindia
+ *
+ */
+public class tutorial_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("testing git");
+
+	}
+
+}
