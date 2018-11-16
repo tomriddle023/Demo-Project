49d71eb9b7039556142e0d5822ff9eff4dc8fae0
tomriddle023 <dark4lord23@gmail.com>
2018-11-16 14:34:45 +0530

    done by prya

---
+++ src/main/java/demo_project/demo_project_mc/tutorial3.java
@@ -0,0 +1,10 @@
+package demo_project.demo_project_mc;
+
+public class tutorial3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("tutoril3");
+	}
+
+}
